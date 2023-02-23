48dd422807e5d22a0b2153f8fbbeadb2cefb55ae
rahim_mazouz <42543749+aspirant2018@users.noreply.github.com>
2023-02-23 20:53:30 +0100

    correcting the functions

--- Similarity.java
+++ Similarity.java
@@ -1,6 +1,9 @@
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Similarity {
 	
@@ -23,22 +26,32 @@ public class Similarity {
 	}
 
 	
-	public static HashMap <String,String> readfiles(){
-		ArrayList<String> listfile = new ArrayList<>();
-		listfile=listFiles("./src/US_Inaugural_Addresses/US_Inaugural_Addresses");
-		for (String file:listfile) {
-			String path= "./src/US_Inaugural_Addresses/US_Inaugural_Addresses"+file;
+	public static HashMap <String,String> readfiles(ArrayList<String> listfile){
+		HashMap <String,String> NameContent = new HashMap <>();
+		for (String namefile:listfile) {
+			String path= "./src/US_Inaugural_Addresses/US_Inaugural_Addresses/"+namefile;
+			System.out.println(path);
+			try {
+				String content = new String(Files.readAllBytes(Paths.get(path)));
+				NameContent.put(namefile, content);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			
 		}
 		
-		return null;
+		return NameContent;
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		readfiles();
 		
-
+		ArrayList<String> listfile = new ArrayList<>();
+		HashMap <String,String> NameContent = new HashMap <>();
+		
+		listfile=listFiles("./src/US_Inaugural_Addresses/US_Inaugural_Addresses");
+		NameContent=readfiles(listfile);
 	}
 
 }
